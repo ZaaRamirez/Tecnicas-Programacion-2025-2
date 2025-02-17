ceddddeccfbae6c28a80d4c983aeb536b8dfddfe
Zaa <zaa_ramirez@outlook.com>
2025-02-17 11:01:20 -0600

    Division

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int numero;
+
+Console.WriteLine("Este programa solamente divide 10 entre un numero positivo entero real");
+
+try
+{
+    numero = int.Parse(Console.ReadLine() ?? "");
+
+    if (numero < 0)
+    {
+        throw new ArgumentException("El numero no puede ser negativo");
+    }
+
+    double division = 10 / numero;
+    Console.WriteLine($"La division de 10 / {numero} es: {division}");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine($"No se puede dividir entre cero");
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("El numero no puede ser negativo");
+}
+catch(FormatException)
+{
+    Console.WriteLine("El numero ingresado no es valido");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Error inesperado: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("El programa terminó");
+}
+
+
+
